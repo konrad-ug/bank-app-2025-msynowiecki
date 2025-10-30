fa86c783fb10a389aa9654c9447d993595ec521e
msynowiecki <m.synowiecki.118@studms.ug.edu.pl>
2025-10-30 11:37:13 +0100

    Laby 2 - Praca na lekcji - spóźniony commit

---
+++ src/personalaccount.py
@@ -0,0 +1,28 @@
+from src.account import Account
+
+class PersonalAccount(Account):
+
+    def __init__(self, first_name, last_name, pesel, promotion_code="Invalid"):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
+        self.balance = 50.0 if self.is_promotion_code_valid(promotion_code) and self.is_born_after_1960(pesel) else 0.0
+        self.promotion_code = promotion_code if self.is_promotion_code_valid(promotion_code) else "Invalid"
+
+    def is_pesel_valid(self, pesel):
+        if isinstance(pesel, str) and len(pesel) == 11 and pesel.isdigit():
+            return True
+        return False
+
+    def is_promotion_code_valid(self, promotion_code):
+        if isinstance(promotion_code, str) and promotion_code.startswith("PROM_") and len(promotion_code) == 8:
+            return True
+        return False
+
+    def is_born_after_1960(self, pesel):
+        if int(pesel[2:4]) <= 12 and int(pesel[:2]) < 60:
+            return True
+        return False
+
+
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -1,9 +1,11 @@
-from src.account import Account
+from src.personalaccount import PersonalAccount
+from src.companyaccount import CompanyAccount
 
 
-class TestAccount:
+class TestPersonalAccount:
+
     def test_account_creation(self):
-        account = Account("John", "Doe", "99039666673")
+        account = PersonalAccount("John", "Doe", "99039666673")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.pesel == "99039666673"
@@ -11,29 +13,95 @@ class TestAccount:
         assert account.promotion_code == "Invalid"
 
     def test_short_pesel(self):
-        account = Account("John", "Doe", "9903966667")
+        account = PersonalAccount("John", "Doe", "9903966667")
         assert account.pesel == "Invalid"
 
     def test_long_pesel(self):
-        account = Account("John", "Doe", "990396666734")
+        account = PersonalAccount("John", "Doe", "990396666734")
         assert account.pesel == "Invalid"
 
     def test_pesel_non_digit(self):
-        account = Account("John", "Doe", None)
+        account = PersonalAccount("John", "Doe", None)
         assert account.pesel == "Invalid"
 
     def test_prom_code_valid(self):
-        account = Account("John", "Doe", "59039666673", "PROM_XYZ")
+        account = PersonalAccount("John", "Doe", "59039666673", "PROM_XYZ")
         assert account.balance == 50.0
         assert account.promotion_code == "PROM_XYZ"
 
     def test_prom_code_valid_but_age(self):
-        account = Account("John", "Doe", "99039666673", "PROM_XYZ")
+        account = PersonalAccount("John", "Doe", "99039666673", "PROM_XYZ")
         assert account.balance == 0.0
         assert account.promotion_code == "PROM_XYZ"
 
     def test_prom_code_invalid(self):
-        account = Account("John", "Doe", "99039666673", "PRM_XYZ")
+        account = PersonalAccount("John", "Doe", "99039666673", "PRM_XYZ")
         assert account.balance == 0.0
         assert account.promotion_code == "Invalid"
+
+    def test_ingoing_transfer_invalid_amount(self):
+        account = PersonalAccount("John", "Doe", "99039666673")
+        assert account.balance == 0.0
+        account.ingoing_transfer(-20.0)
+        assert account.balance == 0.0
+        
+    def test_ingoing_transfer_invalid_type(self):
+        account = PersonalAccount("John", "Doe", "99039666673")
+        assert account.balance == 0.0
+        account.ingoing_transfer('20')
+        assert account.balance == 0.0
+
+
+    def test_ingoing_transfer_valid(self):
+        account = PersonalAccount("John", "Doe", "99039666673")
+        assert account.balance == 0.0
+        account.ingoing_transfer(20.0)
+        assert account.balance == 20.0
+
+    def test_outgoing_transfer_invalid_amount(self):
+        account = PersonalAccount("John", "Doe", "99039666673")
+        assert account.balance == 0.0
+        account.outgoing_transfer(-20.0)
+        assert account.balance == 0.0
+        
+    def test_outgoing_transfer_invalid_type(self):
+        account = PersonalAccount("John", "Doe", "99039666673")
+        assert account.balance == 0.0
+        account.outgoing_transfer('20')
+        assert account.balance == 0.0
+
+    def test_outgoing_transfer_invalid_balance(self):
+        account = PersonalAccount("John", "Doe", "99039666673")
+        assert account.balance == 0.0
+        account.outgoing_transfer(20.0)
+        assert account.balance == 0.0
+
+    def test_outgoing_transfer_valid(self):
+        account = PersonalAccount("John", "Doe", "99039666673")
+        account.ingoing_transfer(30.0)
+        assert account.balance == 30.0
+        account.outgoing_transfer(20.0)
+        assert account.balance == 10.0
+
+class TestCompanyAccount:
+    def test_account_creation(self):
+        account = CompanyAccount("SupCompany", "1021010102")
+        assert account.name == "SupCompany"
+        assert account.nip == "1021010102"
+
+    def test_nip_short(self):
+        account = CompanyAccount("SupCompany", "102101010")
+        assert account.nip == "Invalid"
+
+    def test_nip_long(self):
+        account = CompanyAccount("SupCompany", "10210101022")
+        assert account.nip == "Invalid"
+
+
+
+
+
+        
+
+
 
