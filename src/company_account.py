c547f9300b60731311522593c2a79ef7f32b1a2a
msynowiecki <m.synowiecki.118@studms.ug.edu.pl>
2025-12-14 17:15:48 +0100

    Laby 8 - Początek

--- src/company_account.py
+++ src/company_account.py
@@ -1,19 +1,40 @@
 from src.account import Account
+import requests
+import datetime
+
+url = "https://wl-test.mf.gov.pl/api/search/nip"
+
 
 class CompanyAccount(Account):
+
     def __init__(self, name, nip):
         super().__init__()
         self.name = name
-        self.nip = nip if self.is_nip_valid(nip) else "Invalid"
+        self.nip = nip
         self.express_fee = 5.0
 
+        if self.is_nip_valid(nip):
+            if not self.does_nip_exist(nip):
+                raise ValueError("Company not registered!!")
+
+
     def is_nip_valid(self, nip):
-        if isinstance(nip, str) and len(nip) == 10:
-            return True
+        return isinstance(nip, str) and len(nip) == 10
+
+
+    def does_nip_exist(self, nip):
+        response = requests.get(f'{url}/{nip}?date={datetime.date.today()}')
+        data = response.json()
+
+        if data["result"]["subject"]["statusVat"]:
+            if data["result"]["subject"]["statusVat"] == "Czynny":
+                return True
         return False
+
 
     def outgoing_express_transfer(self, amount):
         return super().outgoing_express_transfer(amount, self.express_fee)
+
 
     def take_loan(self, amount):
         if isinstance(amount, float) and amount > 0 and self.balance >= amount * 2 and -1775.0 in self.history:
